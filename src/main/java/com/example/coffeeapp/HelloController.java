4289656a27155dc2da1157a3f0f4605ff98f8998
JuanDR-19 <juanmanuelduranrueda@gmail.com>
2022-09-16 21:26:22 -0500

    Initial commit

---
+++ src/main/java/com/example/coffeeapp/HelloController.java
@@ -0,0 +1,36 @@
+package com.example.coffeeapp;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class HelloController {
+
+    @FXML
+    private Button inicioSesion;
+
+    @FXML
+    private PasswordField password;
+
+    @FXML
+    private Button registro;
+
+    @FXML
+    private TextField user;
+
+    @FXML
+    void iniciarsesion(ActionEvent event) {
+
+    }
+
+    @FXML
+    void registrarse(ActionEvent event) {
+
+    }
+
+}
