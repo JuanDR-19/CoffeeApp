676a3ed61c4df6e0007cd09ac1f03be3a202ade8
JuanDR-19 <juanmanuelduranrueda@gmail.com>
2022-09-17 23:05:47 -0500

    se cambio la pantalla de inicio y el controlador de CoffeeApp

--- src/main/java/com/example/Controladores/CoffeeAppController.java
+++ src/main/java/com/example/Controladores/CoffeeAppController.java
@@ -1,6 +1,39 @@
 package com.example.Controladores;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.image.ImageView;
+
 public class CoffeeAppController {
 
+    @FXML
+    private Button botonrecetas;
+
+    @FXML
+    private ImageView inicio1;
+
+    @FXML
+    private ListView<?> listaPreparaciones;
+
+    @FXML
+    private Button metodos;
+
+    @FXML
+    private ImageView metodosimg;
+
+    @FXML
+    private ImageView recetasimg;
+
+    @FXML
+    void accesoMetodos(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ingresoRecetas(ActionEvent event) {
+
+    }
 
 }
