52183b223392b30891a0c1e8612337c5154e081c
JuanDR-19 <juanmanuelduranrueda@gmail.com>
2022-09-21 08:18:40 -0500

    pantalla de reacion de cafes

---
+++ src/main/java/com/example/Controladores/AnadirCafesController.java
@@ -0,0 +1,36 @@
+package com.example.Controladores;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class AnadirCafesController {
+
+    @FXML
+    private Button AnadirCafe;
+
+    @FXML
+    private TextField AromaField;
+
+    @FXML
+    private TextField CuerpoField;
+
+    @FXML
+    private TextField MarcaField;
+
+    @FXML
+    private TextField PrecioField;
+
+    @FXML
+    private TextField SaborField;
+
+    @FXML
+    private TextField nameField;
+
+    @FXML
+    void AnadirCafe(ActionEvent event) {
+
+    }
+
+}
--- src/main/java/com/example/Controladores/CoffeeListController.java
+++ src/main/java/com/example/Controladores/CoffeeListController.java
@@ -2,10 +2,16 @@ package com.example.Controladores;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.image.ImageView;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class CoffeeListController {
 
@@ -26,7 +32,18 @@ public class CoffeeListController {
 
     @FXML
     void AnadirCafe(ActionEvent event) {
-
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("AnadirCafes.fxml"));
+        try {
+            Parent root = loader.load();
+            //CoffeeAppController cont = loader.getController();
+            Scene sceneReg = new Scene(root);
+            Stage stageReg = new Stage();
+            stageReg.initModality(Modality.APPLICATION_MODAL);
+            stageReg.setScene(sceneReg);
+            stageReg.showAndWait();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @FXML
