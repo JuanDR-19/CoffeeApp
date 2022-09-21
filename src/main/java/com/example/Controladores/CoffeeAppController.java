4c86877c880650e5086a7e8fbe03f92a0a96923d
JuanDR-19 <juanmanuelduranrueda@gmail.com>
2022-09-21 07:31:07 -0500

    se creo la pantlla de cafes en la pantalla principal

--- src/main/java/com/example/Controladores/CoffeeAppController.java
+++ src/main/java/com/example/Controladores/CoffeeAppController.java
@@ -2,10 +2,16 @@ package com.example.Controladores;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.image.ImageView;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class CoffeeAppController {
 
@@ -42,7 +48,18 @@ public class CoffeeAppController {
 
     @FXML
     void ingresoRecetas(ActionEvent event) {
-        //crear el ingreso a las opciones de las recetas
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("Cafes.fxml"));
+        try {
+            Parent root = loader.load();
+            //CoffeeAppController cont = loader.getController();
+            Scene sceneReg = new Scene(root);
+            Stage stageReg = new Stage();
+            stageReg.initModality(Modality.APPLICATION_MODAL);
+            stageReg.setScene(sceneReg);
+            stageReg.showAndWait();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
---
+++ src/main/java/com/example/Controladores/CoffeeListController.java
@@ -0,0 +1,29 @@
+package com.example.Controladores;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+
+public class CoffeeListController {
+
+    @FXML
+    private ListView<?> ListaCafes;
+
+    @FXML
+    private Button botonVolver;
+
+    @FXML
+    private ImageView cafe1;
+
+    @FXML
+    private ImageView cafe2;
+
+    @FXML
+    void regresarInicio(ActionEvent event) {
+        Stage volverInicioS =  (Stage) botonVolver.getScene().getWindow();
+        volverInicioS.close();
+    }
+
+}
